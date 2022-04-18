3428df8a8ed050b61d1603f539a5aee188f94b69
Muhammad Junaid Khan <65032333+junaidkhan25@users.noreply.github.com>
2022-04-18 18:37:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+
+///// Question1/////
+
+// var number = prompt('Enter positive number')
+// var ifTrue = false
+
+// for(var i = 0 ; i < number.length ; i++){
+//     if(number[i] !== '-'){
+//         document.write('Number: ' + number + '<br/>')
+//         document.write('Round off value: ' + Math.round(number) + '<br/>')
+//         document.write('Floor: ' + Math.floor(number) + '<br/>')
+//         document.write('Ceil: ' + Math.ceil(number) + '<br/>')
+//         ifTrue = true
+//     }
+//     break
+// }
+// if(ifTrue == false){
+//     document.write('PUT POSITIVE NUMBER')
+// }
+
+
+
+// Question2///
+// var number = prompt("Enter negative number");
+// var ifTrue = false
+
+// for(var i = 0 ; i < number.length ; i++){
+//     if(number[i] == "-"){
+//         document.write("Number: " + number + "<br/>");
+//         document.write("Round off value: " + Math.round(number) + "<br/>");
+//         document.write("Floor: " + Math.floor(number) + "<br/>");
+//         document.write("Ceil: " + Math.ceil(number) + "<br/>");
+//         ifTrue = true
+//     }
+//     break
+// }
+// if(ifTrue == false){
+//     document.write("PUT NEGATIVE NUMBER");
+// }
+
+// //Question3////
+// var number = prompt("Enter number");
+//         document.write("Absolute value of " + number + " is " + Math.abs(number) + "<br/>");
+
+
+// ///Question4////
+ //document.write("Random dice value: " + Math.ceil(Math.random() * 6));
+
+
+
+/// // Question5///// /////
+
+// var coin = Math.ceil(Math.random() * 2)
+// if(coin == 1){
+//     document.write("Coin value: Heads")
+// }else{
+//     document.write("Coin value: Tails")
+// }
+
+//  // Question6  /////////  //
+// var forrandom= Math.floor(Math.random()*100);
+ 
+// document.write(forrandom);
+
+
+
+
+// Question8//////////
+
+// var  userinput=+prompt("Enter a number");
+// var result=Math.floor(Math.random()*10);
+ 
+// if(userinput==result){
+//     document.write("CONGRETES!")
+// }
+// else{
+//     document.write("SORRY!")
+// }
